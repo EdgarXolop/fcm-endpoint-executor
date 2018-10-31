d9833cb07496bdd2ecd6a6e66c26c6ea0c660d6f
Edgar Xolop <edgar@kettle.io>
2018-10-31 09:45:11 -0600

    sen to device

---
+++ index.js
@@ -0,0 +1,31 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./credentials.json");
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://<project>.firebaseio.com"
+});
+var registrationToken = "";//your registration token
+
+var message = {
+    notification:{
+        title: 'Hola',
+        body:'puto el que lo lea'
+    },
+    data:{
+        msg:'puta vida pero mas puto Kevin con K'
+    },
+    token: registrationToken
+}
+
+
+admin.messaging().send(message)
+  .then((response) => {
+    // Response is a message ID string.
+    console.log('Successfully sent message:', response);
+  })
+  .catch((error) => {
+    console.log('Error sending message:', error);
+  });
